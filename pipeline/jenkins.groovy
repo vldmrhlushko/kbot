efb8b88f0f2c80f0446b06be2697bb6c7557ae46
Volodymyr Hlushko <vldmrhlushko@gmail.com>
2026-05-13 21:45:45 +0300

    pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,15 @@
 pipeline {
     agent {
         kubernetes {
-            label 'go-build-agent'
-
+            label 'go-custom-agent'
             yaml """
 apiVersion: v1
 kind: Pod
 spec:
   containers:
   - name: go
-    image: golang:1.22
-    command:
-    - cat
+    image: ghcr.io/vldmrhlushko/go-agent:1.0
+    command: ['cat']
     tty: true
 """
         }
@@ -33,29 +31,49 @@ spec:
 
     stages {
 
-        stage('Checkout') {
+        stage("clone") {
             steps {
-                checkout scm
+                echo 'Cloning repository...'
+                  git branch: "${BRANCH}", url: "${REPO}"
             }
         }
 
-        stage('Go version') {
+        stage("test") {
             steps {
-                container('go') {
-                    sh 'go version'
+                echo 'Testing Go installation...'
+                sh 'make test'
                 }
+            }
+
+        stage('build') {
+            steps {
+                echo 'Building Go binary...'
+                sh 'make build'
+            }    
+
+        }
+
+        stage("image") {
+            steps {
+                echo 'Building Docker image...'
+                sh 'make image'
             }
         }
 
-        stage('Build Go binary') {
+        stage("push") {
             steps {
-                container('go') {
-                    sh """
-                        echo "Building for ${params.OS}/${params.ARCH}"
-                        make build TARGETOS=${params.OS} TARGETARCH=${params.ARCH}
-                    """
+                script {
+                    docker.withRegistry('', 'ghcr'https://ghcr.io', 'github') {
+                        sh 'make push'
+                    }
                 }
+                echo 'Pushing Docker image...'
+                sh 'make push'
             }
-        }
-    }
+        }   
+
+
+
+        
+    
 }
