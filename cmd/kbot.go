a3fce0f7a5e84ac340715bff0f81ac349b657ee7
Volodymyr Hlushko <vldmrhlushko@gmail.com>
2026-06-18 13:44:22 +0300

    final sync

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -38,7 +38,7 @@ to quickly create a Cobra application.`,
 		})
 
 		if err != nil {
-			log.Fatal("Please check TELE_TOKEN env variable. %s", err)
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 		}
 
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
